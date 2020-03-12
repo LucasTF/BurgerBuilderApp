e6ed0afe02c70a2861dc5d1fbbd04ba3b8b081c0
Lucas Ferreira <lucas.htferreira@outlook.com>
2020-03-12 14:13:21 -0300

    Changes: Refactored Reducers;

--- src/store/reducers/burgerBuilder.js
+++ src/store/reducers/burgerBuilder.js
@@ -17,46 +17,56 @@ const INGREDIENT_PRICES = {
 const reducer = (state = initialState, action) => {
     switch (action.type) {
         case Action.ADD_INGREDIENT:
-            return updateObject(state, {
-                ingredients: {
-                    ...state.ingredients,
-                    [action.ingredientName]:
-                        state.ingredients[action.ingredientName] + 1,
-                },
-                totalPrice:
-                    state.totalPrice + INGREDIENT_PRICES[action.ingredientName],
-            });
+            return addIngredient(state, action);
         case Action.REMOVE_INGREDIENT:
-            return updateObject(state, {
-                ingredients: {
-                    ...state.ingredients,
-                    [action.ingredientName]:
-                        state.ingredients[action.ingredientName] - 1,
-                },
-                totalPrice:
-                    state.totalPrice - INGREDIENT_PRICES[action.ingredientName],
-            });
+            return removeIngredient(state, action);
         case Action.SET_INGREDIENTS:
-            const calculatedTotalPrice =
-                action.ingredients.salad * INGREDIENT_PRICES['salad'] +
-                action.ingredients.bacon * INGREDIENT_PRICES['bacon'] +
-                action.ingredients.cheese * INGREDIENT_PRICES['cheese'] +
-                action.ingredients.meat * INGREDIENT_PRICES['meat'];
-            return updateObject(state, {
-                ingredients: {
-                    salad: action.ingredients.salad,
-                    bacon: action.ingredients.bacon,
-                    cheese: action.ingredients.cheese,
-                    meat: action.ingredients.meat,
-                },
-                totalPrice: initialState.totalPrice + calculatedTotalPrice,
-                error: false,
-            });
+            return setIngredients(state, action);
         case Action.FETCH_INGREDIENTS_FAILED:
             return updateObject(state, { error: true });
         default:
             return state;
     }
+};
+
+const addIngredient = (state, action) => {
+    const updatedIngredients = updateObject(state.ingredients, {
+        [action.ingredientName]: state.ingredients[action.ingredientName] + 1,
+    });
+    const updatedState = {
+        ingredients: updatedIngredients,
+        totalPrice: state.totalPrice + INGREDIENT_PRICES[action.ingredientName],
+    };
+    return updateObject(state, updatedState);
+};
+
+const removeIngredient = (state, action) => {
+    const updatedIngredients = updateObject(state.ingredients, {
+        [action.ingredientName]: state.ingredients[action.ingredientName] - 1,
+    });
+    const updatedState = {
+        ingredients: updatedIngredients,
+        totalPrice: state.totalPrice - INGREDIENT_PRICES[action.ingredientName],
+    };
+    return updateObject(state, updatedState);
+};
+
+const setIngredients = (state, action) => {
+    const calculatedTotalPrice =
+        action.ingredients.salad * INGREDIENT_PRICES['salad'] +
+        action.ingredients.bacon * INGREDIENT_PRICES['bacon'] +
+        action.ingredients.cheese * INGREDIENT_PRICES['cheese'] +
+        action.ingredients.meat * INGREDIENT_PRICES['meat'];
+    return updateObject(state, {
+        ingredients: {
+            salad: action.ingredients.salad,
+            bacon: action.ingredients.bacon,
+            cheese: action.ingredients.cheese,
+            meat: action.ingredients.meat,
+        },
+        totalPrice: initialState.totalPrice + calculatedTotalPrice,
+        error: false,
+    });
 };
 
 export default reducer;
--- src/store/reducers/order.js
+++ src/store/reducers/order.js
@@ -1,4 +1,5 @@
 import * as Action from '../actions/actionTypes';
+import { updateObject } from '../utils/utility';
 
 const initialState = {
     orders: [],
@@ -9,50 +10,37 @@ const initialState = {
 const reducer = (state = initialState, action) => {
     switch (action.type) {
         case Action.PURCHASE_INIT:
-            return {
-                ...state,
-                purchased: false,
-            };
+            return updateObject(state, { purchased: false });
         case Action.PURCHASE_BURGER_START:
-            return {
-                ...state,
-                loading: true,
-            };
+            return updateObject(state, { loading: true });
         case Action.PURCHASE_BURGER_SUCCESS:
-            const newOrder = {
-                ...action.orderData,
-                id: action.orderId,
-            };
-            return {
-                ...state,
-                loading: false,
-                purchased: true,
-                orders: state.orders.concat(newOrder),
-            };
+            return purchaseBurgerSuccess(state, action);
         case Action.PURCHASE_BURGER_FAILED:
-            return {
-                ...state,
-                loading: false,
-            };
+            return updateObject(state, { loading: false });
         case Action.FETCH_ORDERS_START:
-            return {
-                ...state,
-                loading: true,
-            };
+            return updateObject(state, { loading: true });
         case Action.FETCH_ORDERS_SUCCESS:
-            return {
-                ...state,
+            return updateObject(state, {
                 orders: action.orders,
                 loading: false,
-            };
+            });
         case Action.FETCH_ORDERS_FAILED:
-            return {
-                ...state,
-                loading: false,
-            };
+            return updateObject(state, { loading: false });
         default:
             return state;
     }
+};
+
+const purchaseBurgerSuccess = (state, action) => {
+    const orders = state.orders.concat({
+        ...action.orderData,
+        id: action.orderId,
+    });
+    return updateObject(state, {
+        loading: false,
+        purchased: true,
+        orders: orders,
+    });
 };
 
 export default reducer;
