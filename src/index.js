8b1e51c2178fdd7a53f13d3fc2dd9b6a9470e811
Lucas Ferreira <lucas.htferreira@outlook.com>
2020-04-21 21:31:35 -0300

    Made Redux Dev Tools only available in development

--- src/index.js
+++ src/index.js
@@ -7,16 +7,19 @@ import './index.css';
 import App from './App';
 import thunk from 'redux-thunk';
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const composeEnhancers =
+	process.env.NODE_ENV === 'development'
+		? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+		: null || compose;
 
 const store = createStore(
-    rootReducer,
-    composeEnhancers(applyMiddleware(thunk))
+	rootReducer,
+	composeEnhancers(applyMiddleware(thunk))
 );
 
 ReactDOM.render(
-    <Provider store={store}>
-        <App />
-    </Provider>,
-    document.getElementById('root')
+	<Provider store={store}>
+		<App />
+	</Provider>,
+	document.getElementById('root')
 );
