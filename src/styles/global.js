4c51d153e313250fc0e06c4692c967480e7d4329
Lucas Ferreira <lucas.htferreira@outlook.com>
2020-04-27 12:54:16 -0300

    Changed scrollbar design in global.js

--- src/styles/global.js
+++ src/styles/global.js
@@ -3,8 +3,8 @@ import { createGlobalStyle } from 'styled-components';
 export const GlobalStyle = createGlobalStyle`
 
     :root {
-        --bg-primary: #703b09;
-        --bg-secondary: linear-gradient(#cf8f2e, #bf8129);
+        --bg-primary: linear-gradient(#703b09, #632F07);
+        --bg-secondary: linear-gradient(#cf8f2e, #A06420);
         --bg-tertiary: #daa972;
         --txt-primary: #222;
         --txt-secondary: #f0f0f0;
@@ -19,6 +19,19 @@ export const GlobalStyle = createGlobalStyle`
         margin: 0;
         padding: 0;
         box-sizing: border-box;
+
+        ::-webkit-scrollbar {
+		    width: 0.25rem;
+	    }
+
+	    ::-webkit-scrollbar-track {
+		    background: var(--bg-secondary);
+	    }
+
+
+	    ::-webkit-scrollbar-thumb {
+		    background: var(--bg-primary);
+	    }
     }
 
 `;
