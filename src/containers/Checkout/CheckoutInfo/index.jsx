806854ba0ee031dac99615ce8ac83103338fd82b
Lucas Ferreira <lucas.htferreira@outlook.com>
2020-06-29 17:35:10 -0300

    Refactored CheckoutInfo to functional component

--- src/containers/Checkout/CheckoutInfo/index.jsx
+++ src/containers/Checkout/CheckoutInfo/index.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { connect } from 'react-redux';
 
 import Button from '../../../components/UI/Button';
@@ -15,104 +15,102 @@ import * as Routes from '../../../utils/routes';
 
 import { StyledCheckout } from './styles';
 
-class CheckoutInfo extends Component {
-	state = {
-		orderForm: {
-			name: {
-				elementType: 'input',
-				elementConfig: {
-					type: 'text',
-					label: 'Name:',
-					placeholder: 'Name',
-				},
-				value: '',
-				validation: {
-					required: true,
-					invalidMessage: 'This field cannot be empty.',
-					valid: false,
-				},
-				touched: false,
+const CheckoutInfo = props => {
+	const [orderForm, setOrderForm] = useState({
+		name: {
+			elementType: 'input',
+			elementConfig: {
+				type: 'text',
+				label: 'Name:',
+				placeholder: 'Name',
 			},
-			email: {
-				elementType: 'input',
-				elementConfig: {
-					type: 'email',
-					label: 'Email:',
-					placeholder: 'Email',
-				},
-				value: '',
-				validation: {
-					required: true,
-					invalidMessage: 'This field cannot be empty.',
-					valid: false,
-				},
-				touched: false,
+			value: '',
+			validation: {
+				required: true,
+				invalidMessage: 'This field cannot be empty.',
+				valid: false,
 			},
-			street: {
-				elementType: 'input',
-				elementConfig: {
-					type: 'text',
-					label: 'Street:',
-					placeholder: 'Street',
-				},
-				value: '',
-				validation: {
-					required: true,
-					invalidMessage: 'This field cannot be empty.',
-					valid: false,
-				},
-				touched: false,
-			},
-			cep: {
-				elementType: 'input',
-				elementConfig: {
-					type: 'number',
-					label: 'CEP:',
-					placeholder: 'CEP',
-				},
-				value: '',
-				validation: {
-					required: true,
-					invalidMessage:
-						'This field cannot be empty, must only contain numbers and must have 8 characters.',
-					minLength: 8,
-					maxLength: 8,
-					valid: false,
-				},
-				touched: false,
-			},
+			touched: false,
 		},
-		formIsValid: false,
-	};
+		email: {
+			elementType: 'input',
+			elementConfig: {
+				type: 'email',
+				label: 'Email:',
+				placeholder: 'Email',
+			},
+			value: '',
+			validation: {
+				required: true,
+				invalidMessage: 'This field cannot be empty.',
+				valid: false,
+			},
+			touched: false,
+		},
+		street: {
+			elementType: 'input',
+			elementConfig: {
+				type: 'text',
+				label: 'Street:',
+				placeholder: 'Street',
+			},
+			value: '',
+			validation: {
+				required: true,
+				invalidMessage: 'This field cannot be empty.',
+				valid: false,
+			},
+			touched: false,
+		},
+		cep: {
+			elementType: 'input',
+			elementConfig: {
+				type: 'number',
+				label: 'CEP:',
+				placeholder: 'CEP',
+			},
+			value: '',
+			validation: {
+				required: true,
+				invalidMessage:
+					'This field cannot be empty, must only contain numbers and must have 8 characters.',
+				minLength: 8,
+				maxLength: 8,
+				valid: false,
+			},
+			touched: false,
+		},
+	});
+	const [isFormValid, setIsFormValid] = useState(false);
 
-	orderHandler = event => {
+	const orderHandler = event => {
 		event.preventDefault();
 		const formData = {};
-		for (const formElement in this.state.orderForm) {
-			formData[formElement] = this.state.orderForm[formElement].value;
+		for (const formElement in orderForm) {
+			formData[formElement] = orderForm[formElement].value;
 		}
 		const order = {
-			ingredients: this.props.ingredients,
-			price: this.props.totalPrice,
+			ingredients: props.ingredients,
+			price: props.totalPrice,
 			orderData: formData,
-			userId: this.props.userId,
+			userId: props.userId,
 		};
 
-		this.props.onOrderBurger(order, this.props.token);
+		props.onOrderBurger(order, props.token);
 	};
 
-	inputChangedHandler = (event, id) => {
-		const updatedElement = updateObject(this.state.orderForm[id], {
+	const inputChangedHandler = (event, id) => {
+		const updatedElement = updateObject(orderForm[id], {
 			value: event.target.value,
-			validation: updateObject(this.state.orderForm[id].validation, {
+			validation: updateObject(orderForm[id].validation, {
 				valid: validationHandler(
 					event.target.value,
-					this.state.orderForm[id].validation
+					orderForm[id].validation
 				),
 			}),
 			touched: true,
 		});
-		const updatedOrderForm = updateObject(this.state.orderForm, {
+		const updatedOrderForm = updateObject(orderForm, {
 			[id]: updatedElement,
 		});
 
@@ -123,69 +121,60 @@ class CheckoutInfo extends Component {
 					updatedOrderForm[element].validation.valid &&
 					formIsValid === true;
 		}
-		this.setState({
-			orderForm: updatedOrderForm,
-			formIsValid: formIsValid,
-		});
+		setOrderForm(updatedOrderForm);
+		setIsFormValid(formIsValid);
 	};
 
-	render() {
-		const elementsArray = [];
-		for (const key in this.state.orderForm) {
-			elementsArray.push({
-				id: key,
-				config: this.state.orderForm[key],
-			});
-		}
-		let form = (
-			<form>
-				{elementsArray.map(element => (
-					<Input
-						key={element.id}
-						type={element.config.elementType}
-						name={element.id}
-						placeholder={element.config.elementConfig.placeholder}
-						label={element.config.elementConfig.label}
-						value={element.config.value}
-						invalid={!element.config.validation.valid}
-						touched={element.config.touched}
-						shouldValidate={element.config.validation}
-						invalidMessage={
-							element.config.validation.invalidMessage
-						}
-						onChange={event =>
-							this.inputChangedHandler(event, element.id)
-						}
-					/>
-				))}
-				<Button
-					type='success'
-					click={this.orderHandler}
-					disabled={!this.state.formIsValid}
-				>
-					Finish order
-				</Button>
-				<Button
-					type='danger'
-					click={() => this.props.history.replace(Routes.HOME)}
-				>
-					Cancel
-				</Button>
-			</form>
-		);
-		if (this.props.loading) {
-			form = <Spinner />;
-		}
-		return (
-			<StyledCheckout>
-				<div className='container'>
-					<h4>Enter your information:</h4>
-					{form}
-				</div>
-			</StyledCheckout>
-		);
+	const elementsArray = [];
+	for (const key in orderForm) {
+		elementsArray.push({
+			id: key,
+			config: orderForm[key],
+		});
 	}
-}
+
+	let form = (
+		<form>
+			{elementsArray.map(element => (
+				<Input
+					key={element.id}
+					type={element.config.elementType}
+					name={element.id}
+					placeholder={element.config.elementConfig.placeholder}
+					label={element.config.elementConfig.label}
+					value={element.config.value}
+					invalid={!element.config.validation.valid}
+					touched={element.config.touched}
+					shouldValidate={element.config.validation}
+					invalidMessage={element.config.validation.invalidMessage}
+					onChange={event => inputChangedHandler(event, element.id)}
+				/>
+			))}
+			<Button type='success' click={orderHandler} disabled={!isFormValid}>
+				Finish order
+			</Button>
+			<Button
+				type='danger'
+				click={() => props.history.replace(Routes.HOME)}
+			>
+				Cancel
+			</Button>
+		</form>
+	);
+
+	if (props.loading) {
+		form = <Spinner />;
+	}
+
+	return (
+		<StyledCheckout>
+			<div className='container'>
+				<h4>Enter your information:</h4>
+				{form}
+			</div>
+		</StyledCheckout>
+	);
+};
 
 const mapStateToProps = state => {
 	return {
